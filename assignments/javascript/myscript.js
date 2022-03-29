80b0fbe9f583a4ae1a71f8c3a82e5251bd78948f
madelinemulvey <mulvm252@newschool.edu>
2022-03-28 20:57:14 -0400

    Update myscript.js

--- assignments/javascript/myscript.js
+++ assignments/javascript/myscript.js
@@ -1,2 +1,6 @@
 console.log( document.getElementById("field01").value );
-console.log("hello");
+console.log("helloworld");
+document.getElementById("myButton").onclick = function() {
+  document.getElementById("word01").innerHTML = "WORLD!";
+  document.getElementById("myButton").style.color = "#00ff00";
+}
