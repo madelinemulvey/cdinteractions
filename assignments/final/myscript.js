32156e3226645e293ab6263698c8464cdbbdbf41
madelinemulvey <mulvm252@newschool.edu>
2022-05-13 14:30:07 -0400

    Update myscript.js

--- assignments/final/myscript.js
+++ assignments/final/myscript.js
@@ -12,6 +12,28 @@ function setup() {
 }
   }
 }
+package org.kodejava.swing;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+import java.awt.Cursor;
+
+public class ChangeCursor extends JFrame {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            ChangeCursor frame = new ChangeCursor();
+            frame.setTitle("Change Cursor");
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            frame.setSize(500, 500);
+
+            // Here we create a hand shaped cursor!
+            Cursor cursor = new Cursor(Cursor.HAND_CURSOR);
+            frame.setCursor(cursor);
+            frame.setVisible(true);
+        });
+    }
+}
 
 
 
