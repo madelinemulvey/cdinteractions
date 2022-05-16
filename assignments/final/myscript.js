71abee7f5151573b3312d7c0440803230da2efac
madelinemulvey <mulvm252@newschool.edu>
2022-05-15 20:16:14 -0400

    Update myscript.js

--- assignments/final/myscript.js
+++ assignments/final/myscript.js
@@ -1,18 +1,29 @@
 
 
+  const buttons = document.querySelectorALL('.btn')
+  const storeImages = document.querySelectorALL('.store-item')
 
+  buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      e.preventDefault()
+      const filter = e.target.dataset.filter
 
+      storeImages.forEach((item) => {
+        if (filter === 'all') {
+          item.style.display = 'block'
+        } else {
+          if (item.classList.contains(filter)) {
+            item.style.display = 'block'
+          } else {
+             item.style.display = 'none'
 
+           }
 
+          }
 
-
-
-}
-  }
-}
-
-
-
+      })
+    })
+    });
 
 
 
